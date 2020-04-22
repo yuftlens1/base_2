1dd92a4a5ce699c661da63d05b58677177f5b469
yuftlens1 <1606059673yft@gmail.com>
2020-04-22 16:38:09 +0800

    基础第二阶段_文件复制

--- 文件操作之复制备份.py
+++ 文件操作之复制备份.py
@@ -1,3 +1,7 @@
+# f1 = open('123.maq.txt','w')   #创建一个文件
+# f1.write('qwerasdf')           #往里写点东西
+# f1.close()
+
 import time
 time1 = time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
 old_name = '123.maq.txt'
@@ -9,3 +13,14 @@ print(old_name[file1:])             #取出文件名右边的第一个 '.'的下
 
 new_name = old_name[0:file1] + '_bak_' +time1 + old_name[file1:]
 print(new_name)
+
+old_f = open(old_name,'rb')    #以二进制打开，从底层打开，一般没啥问题。
+new_f = open(new_name,'wb')    #以二进制打开，以二进制写入。
+while True:                    #循环读写。
+    con = old_f.read(1024)     #1024表示读取长度。每次循环读取这么长
+    # con = old_f.read(1)      #1表示读取长度。
+    if len(con) == 0:
+        break
+    new_f.write(con)
+new_f.close()
+old_f.close()
