7216a2afa585f27a733d020db3e092b04a6f1ce0
yuftlens1 <1606059673yft@gmail.com>
2020-04-22 18:50:25 +0800

    基础第二阶段_文件复制

--- 文件操作之复制备份.py
+++ 文件操作之复制备份.py
@@ -4,23 +4,28 @@
 
 import time
 time1 = time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
-old_name = '123.maq.txt'
+old_name = '.txt'
 file1 = old_name.rfind('.')         #rfind  查找字符串，从右边找指定字符下标。
 print(file1)    #结果  7    ##从右边找到的第一个 '.'  ,然后返回其下标。
 
-print(old_name[0:file1])            #取出文件名开始下标到右边的第一个 '.'的名字字段。
-print(old_name[file1:])             #取出文件名右边的第一个 '.'的下标到结尾的名字字段。
+if file1 > 0:    #判断，如果只有 .后缀  的，则不执行复制。例如文件名只是 '.txt'。还有一个写法实现，看下面。
+    # postfix = old_name[file1:]  #post有'后'的意思。
 
-new_name = old_name[0:file1] + '_bak_' +time1 + old_name[file1:]
-print(new_name)
+    print(old_name[0:file1])            #取出文件名开始下标到右边的第一个 '.'的名字字段。
+    print(old_name[file1:])             #取出文件名右边的第一个 '.'的下标到结尾的名字字段。
 
-old_f = open(old_name,'rb')    #以二进制打开，从底层打开，一般没啥问题。
-new_f = open(new_name,'wb')    #以二进制打开，以二进制写入。
-while True:                    #循环读写。
-    con = old_f.read(1024)     #1024表示读取长度。每次循环读取这么长
-    # con = old_f.read(1)      #1表示读取长度。
-    if len(con) == 0:
-        break
-    new_f.write(con)
-new_f.close()
-old_f.close()
+    new_name = old_name[0:file1] + '_bak_' +time1 + old_name[file1:]
+    print(new_name)
+
+    old_f = open(old_name,'rb')    #以二进制打开，从底层打开，一般没啥问题。
+    new_f = open(new_name,'wb')    #以二进制打开，以二进制写入。
+    while True:                    #循环读写。
+        con = old_f.read(1024)     #1024表示读取长度。每次循环读取这么长
+        # con = old_f.read(1)      #1表示读取长度。
+        if len(con) == 0:
+            break
+        new_f.write(con)          #把读到的写进来。
+    new_f.close()
+    old_f.close()
+else:
+    print(f'{old_name} 是无效文件')
