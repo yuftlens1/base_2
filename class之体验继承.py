ff9987e10cf6ecd1b6fc74c2a7e6760d53cd39d3
yuftlens1 <1606059673yft@gmail.com>
2020-05-07 18:43:50 +0800

    基础第二阶段_calss继承

--- class之体验继承.py
+++ class之体验继承.py
@@ -2,15 +2,17 @@
 
 #1.定义一个类(父类)
 class A(object):
-# class A():     #在python中，所有类默认继承object类。object类是顶级类或基类；其他子类叫做派生类。
+# class A():     ##在python中，所有类默认继承object类。object类是顶级类或基类；其他子类叫做派生类。
     def __init__(self):
         self.num = 2
     def print_info(self):
         print(self.num)
 
-class B(A):
+class B(A):     #子类B 继承class A (单继承)
     pass
 
-test1 = B()         #class实例化对象 不要忘记 ()     #子类也叫派生类。
+test1 = B()         #子class B 实例化对象 不要忘记 ()     #子类也叫派生类。
 
-test1.print_info()
+test1.print_info()   #对象调用子 class内方法。
+
+print(test1.num)   #直接打印子类B属性。
