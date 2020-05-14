50e416e3da18c78a64a0e2b671e635709a377848
yuftlens1 <1606059673yft@gmail.com>
2020-05-14 22:16:12 +0800

    python3基础_class之多态

--- 面向对象之其他零散知识点.py
+++ 面向对象之其他零散知识点.py
@@ -11,3 +11,33 @@
 '''
 #python的多态不是必须依赖于继承，是最好依赖于继承。
 #定义:多态是一种使用对象的方式，子类重写父类方法，调用不同子类对象的相同父类方法，可以产生不同的执行结果。
+
+# 多态的创建使用
+#     创建父类，定义公共方法
+#     定义子类，子类重写父类方法
+#     创建对象，调用不同的功能，传入不同的对象，观察执行的结果
+
+#1    创建父类，定义公共方法      #警犬和人
+class Dog(object):
+    def work(self):
+        pass
+#2    定义子类，子类重写父类方法   #定义两类表示不同的警犬
+class Armydog(Dog):                                        #多态最好依赖于继承。 #子类Armydog继承父类Dog
+    def work(self):
+        print('追击敌人')
+class Drugdog(Dog):                                        #多态最好依赖于继承。 #子类Drugdog继承父类Dog
+    def work(self):
+        print('搜寻毒品')
+
+class Person(object):
+    def work_with_dog(self,dog):
+        dog.work()#这里必须是work。因为下面是把两个Dog的子类(Armydog和Drugdog)作为这里的形参(dog)传入。而Armydog和Drugdog都只有work一个方法。参考'面向对象实战之搬家具.py'
+
+#3    创建对象，调用不同的功能，传入不同的对象，观察执行的结果
+ad = Armydog()
+dd = Drugdog()
+sir = Person()
+ad.work()
+sir.work_with_dog(ad)
+sir.work_with_dog(dd)
+
