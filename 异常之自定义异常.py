f5a033a0281ad322203bc4d3070c11ae0de9a0c2
yuftlens1 <1606059673yft@gmail.com>
2020-05-19 19:18:49 +0800

    python3基础_自定义捕获异常

--- 异常之自定义异常.py
+++ 异常之自定义异常.py
@@ -1,4 +1,4 @@
-# 自定义异常类，继承Exception
+# 自定义异常类，继承Exception            #就是自定义错误情况，把这些错用raise抛出到自定义类(str)里面输出给用户看。！！！
 class ShortIonputError(Exception):
     def __init__(self,length,min_len):
         self.length = length
@@ -6,4 +6,17 @@ class ShortIonputError(Exception):
     def __str__(self):
         return f'你输入的长度是{self.length},不能少于{self.min_len}个字符'
 
-print(ShortIonputError(1,3))   #raise和print很像。
+# print(ShortIonputError(1,3))   #raise和print很像。
+
+def main():                                                 #自定义错误情况
+    try:
+        password = input('请输入密码')
+        if len(password) < 4:                               #如果此处成立，则执行下面的 raise 抛出。
+            raise ShortIonputError(len(password),3)         #raise 调用异常类。
+
+    except Exception as  result:
+        print(result)
+    else:
+        print('无异常') #try下的无异常代码后，执行else。 如果 "if len(password) < 4:" 成立，就去调用抛出异常的raise了。
+
+main()
