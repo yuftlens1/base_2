6d698a6789cc3beb8f2e3a00448997b698cd3e45
yuftlens1 <1606059673yft@gmail.com>
2020-10-14 13:54:01 +0800

    文件操作小复习

--- py查看目录文件.py
+++ py查看目录文件.py
@@ -1,7 +1,7 @@
 import os
-print (os.getcwd())
-path = os.getcwd()
-print(os.listdir(path))
-f = open('文件操作之read.py',encoding='UTF-8')
-print(f.read())
-f.close()
+print(os.getcwd())                        #打印出当前路径。
+path = os.getcwd()                        #把当前路径附在path里方便后续操作。
+print(os.listdir(path))                   #打印当前路径里的所有文件。
+f = open('文件操作之read.py',encoding='UTF-8')       #打开指定文件，指定utf-8字符集
+print(f.read())                                    #打印文件里内容
+f.close()                                          #关闭打开的文件
